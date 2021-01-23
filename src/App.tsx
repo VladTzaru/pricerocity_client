417157cdccc9a101a95ee0cc699b428e921c63a2
VladTzaru <bvanja.87@gmail.com>
2021-01-23 18:02:33 +0100

    Added InvoiceR1 component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 
 // Custom components imports
 import Header from "./components/Header";
+import InvoiceR1 from "./components/forms/InvoiceR1";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Switch>
+        <Route component={InvoiceR1} path='/forms/invoice_r1' />
+      </Switch>
     </>
   );
 };
---
+++ src/components/forms/InvoiceR1.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InvoiceR1 = () => {
+  return (
+    <div>
+      <h1>I AM INVOTE R1</h1>
+    </div>
+  );
+};
+
+export default InvoiceR1;

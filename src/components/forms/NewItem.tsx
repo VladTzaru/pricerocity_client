c688abac4e2440587ca1e65a8689f78a33a334d1
VladTzaru <bvanja.87@gmail.com>
2021-01-26 19:50:42 +0100

    Added addNewItem post method

--- src/components/forms/NewItem.tsx
+++ src/components/forms/NewItem.tsx
@@ -3,6 +3,7 @@ import { Formik, Form } from "formik";
 import InputField from "../InputField";
 import { Button, FormGroup, Header } from "semantic-ui-react";
 import * as Yup from "yup";
+import axios from "axios";
 
 interface Values {
   itemNameCro: string;
@@ -25,35 +26,71 @@ const validationSchema = Yup.object().shape({
   vat: Yup.number().required("PDV je obavezno uneti"),
 });
 
-const NewItem = () => (
-  <Formik
-    validationSchema={validationSchema}
-    initialValues={initialValues}
-    onSubmit={(values) => console.log(values)}
-  >
-    {({ dirty, isValid }) => (
-      <Form className='ui form'>
-        <Header as='h2'>Opis Stavke</Header>
-        <FormGroup widths={2}>
-          <InputField name='itemNameCro' label='Naziv proizvoda (HRV)' />
-          <InputField name='itemNameEng' label='Naziv Proizvoda (ENG)' />
-        </FormGroup>
-        <FormGroup widths={2}>
-          <InputField type='number' name='retailPrice' label='MPC' />
-          <InputField type='number' name='vat' label='PDV' />
-        </FormGroup>
+const NewItem = () => {
+  const addNewItem = async (item: Values) => {
+    try {
+      await axios.post(`${process.env.REACT_APP_API}/item/new`, item, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        <Button
-          primary
-          size='large'
-          disabled={!dirty || !isValid}
-          type='submit'
-        >
-          Dodaj stavku
-        </Button>
-      </Form>
-    )}
-  </Formik>
-);
+  return (
+    <Formik
+      validationSchema={validationSchema}
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        addNewItem(values);
+        actions.resetForm();
+      }}
+    >
+      {({ dirty, isValid, isSubmitting }) => (
+        <Form className='ui form'>
+          <Header as='h2'>Opis Stavke</Header>
+          <FormGroup widths={2}>
+            <InputField
+              disabled={isSubmitting}
+              name='itemNameCro'
+              label='Naziv proizvoda (HRV)'
+            />
+            <InputField
+              disabled={isSubmitting}
+              name='itemNameEng'
+              label='Naziv Proizvoda (ENG)'
+            />
+          </FormGroup>
+          <FormGroup widths={2}>
+            <InputField
+              disabled={isSubmitting}
+              type='number'
+              name='retailPrice'
+              label='MPC'
+            />
+            <InputField
+              disabled={isSubmitting}
+              type='number'
+              name='vat'
+              label='PDV'
+            />
+          </FormGroup>
+
+          <Button
+            loading={isSubmitting}
+            primary
+            size='large'
+            disabled={isSubmitting || !dirty || !isValid}
+            type='submit'
+          >
+            Dodaj stavku
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
 export default NewItem;

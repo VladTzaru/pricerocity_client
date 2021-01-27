584bdd50dfe1b5855092566c29f2a65ba8918da5
VladTzaru <bvanja.87@gmail.com>
2021-01-27 10:41:21 +0100

    Added ItemListPage component

--- src/pages/ItemListPage.tsx
+++ src/pages/ItemListPage.tsx
@@ -1,30 +1,56 @@
-import axios, { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { List } from "semantic-ui-react";
+import { LOADING } from "../constants";
 import { Item, MatchParams } from "../types";
 import { replaceStringChunk } from "../utility/utils";
 
 const ItemListPage: React.FC<RouteComponentProps<MatchParams>> = ({
   match,
 }) => {
-  const [items, setItems] = useState<AxiosResponse<Item[]> | null>(null);
+  const [items, setItems] = useState<Item[]>([]);
   const { url } = match;
   const searchTerm = replaceStringChunk(url, "/");
 
-  useEffect(() => {
-    if (!items) {
-      const getItems = async () => {
-        const response = await axios.get<Item[]>(
-          `${process.env.REACT_APP_API}/${searchTerm}`
-        );
-        setItems(response);
-      };
-      getItems();
+  const renderContent = () => {
+    if (items.length === 0) {
+      return <h4>{LOADING}</h4>;
     }
-  }, [items, searchTerm]);
+
+    return (
+      <List divided relaxed>
+        {items.map((item) => (
+          <List.Item key={item.id}>
+            <List.Icon
+              name='product hunt'
+              size='large'
+              verticalAlign='middle'
+            />
+            <List.Content>
+              <List.Header as='a'>{item.itemNameCro}</List.Header>
+              <List.Description as='a'>{item.createdAt}</List.Description>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
+    );
+  };
+
+  useEffect(() => {
+    const getItems = async () => {
+      const { data } = await axios.get<Item[]>(
+        `${process.env.REACT_APP_API}/${searchTerm}`
+      );
+
+      setItems(data);
+    };
+    getItems();
+  }, [searchTerm]);
   return (
     <div>
-      <h1>ITEM LIST</h1>
+      <h1>Sve stavke</h1>
+      {renderContent()}
     </div>
   );
 };

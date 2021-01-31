98027ba9a49a6b557326e66fe55eb5945fec9cb8
VladTzaru <bvanja.87@gmail.com>
2021-01-31 18:48:08 +0100

    Added company info

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,8 @@
 export const LOADING = "Učitavanje...";
 export const NO_CONTENT = "Nema sadržaja.";
+export const PLASINIA_INFORMATION = `T: +385 91 988 9472 | E: info@plasinia.hr | Trgovački sud u
+Zagrebu. Temeljni kapital: 20.000,00 HRK (kuna). Direktorica:
+Ivanka Đelilović | MBS: 080863167 | IBAN: HR9723400091110950153
+PLASINIA`;
+export const PLASINIA_ADDRESS =
+  "PLASINIA d.o.o. Vratnička 6 Zagreb, 10 040, Hrvatska OIB: 95470441689";
--- src/pages/PrintPage.tsx
+++ src/pages/PrintPage.tsx
@@ -1,11 +1,34 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { PLASINIA_ADDRESS, PLASINIA_INFORMATION } from "../constants";
+
 const PrintPage = () => {
   return (
     <>
       <div className='page'>
-        <div className='subpage'>Page 1/2</div>
+        <div className='subpage'>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={16}>{PLASINIA_INFORMATION}</Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column floated='left' width={5}>
+                {PLASINIA_ADDRESS}
+              </Grid.Column>
+              <Grid.Column floated='right' width={5}>
+                PLASINIA
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
       </div>
     </>
   );
 };
+
+// PLASINIA d.o.o.
+// Vratnička 6
+// Zagreb, 10 040, Hrvatska
+// OIB: 95470441689
 
 export default PrintPage;

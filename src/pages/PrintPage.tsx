b337f4e3a46bb0cbc698c42768ecd01310546181
VladTzaru <bvanja.87@gmail.com>
2021-02-01 12:17:15 +0100

    Updated print page

--- src/pages/PrintPage.tsx
+++ src/pages/PrintPage.tsx
@@ -1,34 +1,46 @@
 import React from "react";
-import { Grid } from "semantic-ui-react";
+import { useLocation } from "react-router-dom";
+import { Grid, Header } from "semantic-ui-react";
 import { PLASINIA_ADDRESS, PLASINIA_INFORMATION } from "../constants";
+import { useInvoice } from "../store/invoice";
+import { InvoiceR1 } from "../types";
+import { replaceStringChunk } from "../utility/utils";
 
-const PrintPage = () => {
-  return (
-    <>
-      <div className='page'>
-        <div className='subpage'>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={16}>{PLASINIA_INFORMATION}</Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column floated='left' width={5}>
-                {PLASINIA_ADDRESS}
-              </Grid.Column>
-              <Grid.Column floated='right' width={5}>
-                PLASINIA
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </div>
-      </div>
-    </>
-  );
+const selectInvoice = (id: string, list: InvoiceR1[]): InvoiceR1 => {
+  const invoice = list.filter((list) => list._id === id);
+  return invoice[0];
 };
 
-// PLASINIA d.o.o.
-// Vratnička 6
-// Zagreb, 10 040, Hrvatska
-// OIB: 95470441689
+const PrintPage = () => {
+  const { selectedInvoices } = useInvoice();
+  const { pathname } = useLocation();
+  const invoiceId = replaceStringChunk(pathname, "/print/");
+  const selectedInvoice = selectInvoice(invoiceId, selectedInvoices);
+
+  return (
+    <div className='page'>
+      <div className='subpage'>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={16}>{PLASINIA_INFORMATION}</Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column floated='left' width={5}>
+              {PLASINIA_ADDRESS}
+            </Grid.Column>
+            <Grid.Column floated='right' width={5}>
+              PLASINIA
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={16}>
+              <Header as='h2'>{selectedInvoice.buyerName} </Header>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    </div>
+  );
+};
 
 export default PrintPage;

8279f70e3eb869893acb485114352c34870ed12f
VladTzaru <bvanja.87@gmail.com>
2021-02-06 16:27:57 +0100

    Update print page style

--- src/pages/PrintPage.tsx
+++ src/pages/PrintPage.tsx
@@ -43,169 +43,162 @@ const PrintPage = () => {
   return (
     <>
       <SideMenu />
-      <div className='page'>
-        <div className='subpage'>
-          <Grid>
-            {/* GENERAL INFO */}
+      <div className='print'>
+        <Grid>
+          {/* GENERAL INFO */}
+          <Grid.Row>
+            <Grid.Column width={16}>
+              <p className='small-text'>{PLASINIA_INFORMATION}</p>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column floated='left' width={10}>
+              <p className='small-text'>{PLASINIA_ADDRESS}</p>
+            </Grid.Column>
+            <Grid.Column floated='right' width={6}>
+              PLASINIA
+            </Grid.Column>
+          </Grid.Row>
+
+          {/* DOCUMENT NAME */}
+          <Grid.Row>
+            <Grid.Column width={16}>
+              <Header as='h3'>
+                {selectedInvoice?.documentType === DocumentType.INVOICE_R1
+                  ? INVOICE_R1
+                  : selectedInvoice?.documentType}
+              </Header>
+            </Grid.Column>
+          </Grid.Row>
+
+          {/* DOCUMENT INFO */}
+          <Grid.Row>
+            <Grid.Column floated='left' width={16}>
+              DATUM: {formatDate(selectedInvoice.date, DateFormat.MM_DD_YYYY)}
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column floated='left' width={5}>
+              <p className='small-text'>KUPAC: {formatBuyer()}</p>
+            </Grid.Column>
+            <Grid.Column floated='right' width={5}>
+              <p className='small-text'>PRIMAOC: {formatBuyer()}</p>
+            </Grid.Column>
+            <Grid.Column floated='right' width={6}>
+              <p className='small-text'>
+                TIP RAČUNA: {selectedInvoice.documentType}
+              </p>
+              <p className='small-text'>
+                BROJ RAČUNA:{" "}
+                {`${selectedInvoice.invoiceNumberPrefix}/${selectedInvoice.invoiceNumberSuffix}`}
+              </p>
+              <p className='small-text'>
+                VALUTA RAČUNA: {selectedInvoice.paymentDeadlineDate}
+              </p>
+              <p className='small-text'>
+                NAČIN PLAĆANJA: {selectedInvoice.paymentMethod}
+              </p>
+              <p className='small-text'>
+                RAČUN IZDAT:{" "}
+                {formatDate(selectedInvoice.invoiceIssuedAt, DateFormat.HH_mm)}
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+
+          {/* ITEM LIST */}
+
+          <Grid.Row>
+            <Grid.Column floated='left' width={16}>
+              <Table
+                striped
+                stackable
+                compact
+                size='small'
+                singleLine
+                definition
+              >
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>#</Table.HeaderCell>
+                    <Table.HeaderCell>Naziv proizvoda</Table.HeaderCell>
+                    <Table.HeaderCell>Količina</Table.HeaderCell>
+                    <Table.HeaderCell>PDV</Table.HeaderCell>
+                    <Table.HeaderCell>Cijena</Table.HeaderCell>
+                    <Table.HeaderCell>Popust</Table.HeaderCell>
+                    <Table.HeaderCell>Cijena s popustom</Table.HeaderCell>
+                    <Table.HeaderCell>Ukupno</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  {selectedInvoice.items.map((item, i) => {
+                    return (
+                      <Table.Row key={item.id}>
+                        <Table.Cell>{i + 1}</Table.Cell>
+                        <Table.Cell>{item.itemName}</Table.Cell>
+                        <Table.Cell>
+                          {item.quantity} {item.unit}
+                        </Table.Cell>
+                        <Table.Cell>{item.vat}%</Table.Cell>
+                        <Table.Cell>{item.retailPrice} kn</Table.Cell>
+                        <Table.Cell>{item.discount}%</Table.Cell>
+                        <Table.Cell>
+                          {roundTo2Digits(
+                            item.retailPrice * ((100 - item.discount) / 100)
+                          )}{" "}
+                          kn
+                        </Table.Cell>
+                        <Table.Cell>
+                          {roundTo2Digits(
+                            item.quantity *
+                              (item.retailPrice * ((100 - item.discount) / 100))
+                          )}
+                        </Table.Cell>
+                      </Table.Row>
+                    );
+                  })}
+                </Table.Body>
+              </Table>
+            </Grid.Column>
+          </Grid.Row>
+
+          {/* Info about invoice and responsible person and totals */}
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <p className='small-text'>{PLASINIA_TAX_REGULATIONS}</p>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <p className='small-text'>
+                DOSTAVA: {selectedInvoice.summary.shipping}
+              </p>
+              <p className='small-text'>
+                UKUPNO: {selectedInvoice.summary.totalWithoutVat}
+              </p>
+              <p className='small-text'>
+                PDV: {selectedInvoice.summary.totalVat}
+              </p>
+              <p className='small-text'>
+                UKUPNO S PDV-om: {selectedInvoice.summary.totalWithVAT}
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+
+          {/* FOOTER */}
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <p className='small-text'>ZA PLAĆANJE</p>
+              <p className='small-text'>{PLASINIA_FOR_PAYMENT}</p>
+            </Grid.Column>
+          </Grid.Row>
+          {selectedInvoice.notes && (
             <Grid.Row>
               <Grid.Column width={16}>
-                <p className='small-text'>{PLASINIA_INFORMATION}</p>
+                <p className='small-text'>NAPOMENE</p>
+                <p className='small-text'>{selectedInvoice.notes}</p>
               </Grid.Column>
             </Grid.Row>
-            <Grid.Row>
-              <Grid.Column floated='left' width={10}>
-                <p className='small-text'>{PLASINIA_ADDRESS}</p>
-              </Grid.Column>
-              <Grid.Column floated='right' width={6}>
-                PLASINIA
-              </Grid.Column>
-            </Grid.Row>
-
-            {/* DOCUMENT NAME */}
-            <Grid.Row>
-              <Grid.Column width={16}>
-                <Header as='h3'>
-                  {selectedInvoice?.documentType === DocumentType.INVOICE_R1
-                    ? INVOICE_R1
-                    : selectedInvoice?.documentType}
-                </Header>
-              </Grid.Column>
-            </Grid.Row>
-
-            {/* DOCUMENT INFO */}
-            <Grid.Row>
-              <Grid.Column floated='left' width={16}>
-                DATUM: {formatDate(selectedInvoice.date, DateFormat.MM_DD_YYYY)}
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column floated='left' width={5}>
-                <p className='small-text'>KUPAC: {formatBuyer()}</p>
-              </Grid.Column>
-              <Grid.Column floated='right' width={5}>
-                <p className='small-text'>PRIMAOC: {formatBuyer()}</p>
-              </Grid.Column>
-              <Grid.Column floated='right' width={6}>
-                <p className='small-text'>
-                  TIP RAČUNA: {selectedInvoice.documentType}
-                </p>
-                <p className='small-text'>
-                  BROJ RAČUNA:{" "}
-                  {`${selectedInvoice.invoiceNumberPrefix}/${selectedInvoice.invoiceNumberSuffix}`}
-                </p>
-                <p className='small-text'>
-                  VALUTA RAČUNA: {selectedInvoice.paymentDeadlineDate}
-                </p>
-                <p className='small-text'>
-                  NAČIN PLAĆANJA: {selectedInvoice.paymentMethod}
-                </p>
-                <p className='small-text'>
-                  RAČUN IZDAT:{" "}
-                  {formatDate(
-                    selectedInvoice.invoiceIssuedAt,
-                    DateFormat.HH_mm
-                  )}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-
-            {/* ITEM LIST */}
-
-            <Grid.Row>
-              <Grid.Column floated='left' width={16}>
-                <Table
-                  striped
-                  stackable
-                  compact
-                  size='small'
-                  singleLine
-                  collapsing
-                  definition
-                >
-                  <Table.Header>
-                    <Table.Row>
-                      <Table.HeaderCell>#</Table.HeaderCell>
-                      <Table.HeaderCell>Naziv proizvoda</Table.HeaderCell>
-                      <Table.HeaderCell>Količina</Table.HeaderCell>
-                      <Table.HeaderCell>PDV</Table.HeaderCell>
-                      <Table.HeaderCell>Cijena</Table.HeaderCell>
-                      <Table.HeaderCell>Popust</Table.HeaderCell>
-                      <Table.HeaderCell>Cijena s popustom</Table.HeaderCell>
-                      <Table.HeaderCell>Ukupno</Table.HeaderCell>
-                    </Table.Row>
-                  </Table.Header>
-
-                  <Table.Body>
-                    {selectedInvoice.items.map((item, i) => {
-                      return (
-                        <Table.Row key={item.id}>
-                          <Table.Cell>{i + 1}</Table.Cell>
-                          <Table.Cell>{item.itemName}</Table.Cell>
-                          <Table.Cell>
-                            {item.quantity} {item.unit}
-                          </Table.Cell>
-                          <Table.Cell>{item.vat}%</Table.Cell>
-                          <Table.Cell>{item.retailPrice} kn</Table.Cell>
-                          <Table.Cell>{item.discount}%</Table.Cell>
-                          <Table.Cell>
-                            {roundTo2Digits(
-                              item.retailPrice * ((100 - item.discount) / 100)
-                            )}{" "}
-                            kn
-                          </Table.Cell>
-                          <Table.Cell>
-                            {roundTo2Digits(
-                              item.quantity *
-                                (item.retailPrice *
-                                  ((100 - item.discount) / 100))
-                            )}
-                          </Table.Cell>
-                        </Table.Row>
-                      );
-                    })}
-                  </Table.Body>
-                </Table>
-              </Grid.Column>
-            </Grid.Row>
-
-            {/* Info about invoice and responsible person and totals */}
-            <Grid.Row>
-              <Grid.Column width={10}>
-                <p className='small-text'>{PLASINIA_TAX_REGULATIONS}</p>
-              </Grid.Column>
-              <Grid.Column width={6}>
-                <p className='small-text'>
-                  DOSTAVA: {selectedInvoice.summary.shipping}
-                </p>
-                <p className='small-text'>
-                  UKUPNO: {selectedInvoice.summary.totalWithoutVat}
-                </p>
-                <p className='small-text'>
-                  PDV: {selectedInvoice.summary.totalVat}
-                </p>
-                <p className='small-text'>
-                  UKUPNO S PDV-om: {selectedInvoice.summary.totalWithVAT}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-
-            {/* FOOTER */}
-            <Grid.Row>
-              <Grid.Column width={10}>
-                <p className='small-text'>ZA PLAĆANJE</p>
-                <p className='small-text'>{PLASINIA_FOR_PAYMENT}</p>
-              </Grid.Column>
-            </Grid.Row>
-            {selectedInvoice.notes && (
-              <Grid.Row>
-                <Grid.Column width={16}>
-                  <p className='small-text'>NAPOMENE</p>
-                  <p className='small-text'>{selectedInvoice.notes}</p>
-                </Grid.Column>
-              </Grid.Row>
-            )}
-          </Grid>
-        </div>
+          )}
+        </Grid>
       </div>
     </>
   );
